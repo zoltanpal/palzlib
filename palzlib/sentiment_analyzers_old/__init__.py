6d25e9d8e485fce1f38af3a821aa5cb2e116973b
Zoltan Pal <palzoltanet@gmail.com>
2025-03-12 21:14:24 +0100

    refactoring adding palzlib

--- palzlib/sentiment_analyzers_old/__init__.py
+++
@@ -1,8 +0,0 @@
-# from palzlib.sentiment_analyzers_old.langs.eng.sentiment_analyzer import (
-#     get_sentiments as get_sentiments_eng,
-# )
-from palzlib.sentiment_analyzers_old.langs.hun.sentiment_analyzer import (
-    get_sentiments as get_sentiments_hun,
-)
-
-__all__ = [ "get_sentiments_hun", "get_sentiments_dan"]
