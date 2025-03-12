a0e884848de064e2cab13bf8eb464ef88ac6c4ab
Zoltan Pal <zopal@pandora.net>
2025-03-12 12:22:00 +0100

    add docstrings

--- palzlib/sentiment_analyzers/factory/sentiment_factory.py
+++ palzlib/sentiment_analyzers/factory/sentiment_factory.py
@@ -1,9 +1,14 @@
 from palzlib.sentiment_analyzers.analyzers.dan.sentiment_analyzer import DanishSentimentAnalyzer
 from palzlib.sentiment_analyzers.analyzers.eng.sentiment_analyzer import EnglishSentimentAnalyzer
 from palzlib.sentiment_analyzers.analyzers.hun.sentiment_analyzer import HungarianSentimentAnalyzer
-#from palzlib.sentiment_analyzers.analyzers.eng.sentiment_analyzer import EnglishSentimentAnalyzer
 
 class SentimentAnalyzerFactory:
+    """
+    A factory class that provides access to language-specific sentiment analyzers.
+
+    This class is used to get the appropriate sentiment analyzer instance based on the specified language.
+    It supports English (eng), Danish (dan), and Hungarian (hun) analyzers.
+    """
     _analyzers = {
         "hun": HungarianSentimentAnalyzer(),
         "dan": DanishSentimentAnalyzer(),
@@ -12,6 +17,15 @@ class SentimentAnalyzerFactory:
 
     @staticmethod
     def get_analyzer(language: str):
+        """
+        Retrieves the sentiment analyzer for the specified language.
+
+        Args:
+            language (str): The language code for which the sentiment analyzer is requested.
+                            Supported values are 'hun' (Hungarian), 'dan' (Danish), and 'eng' (English).
+        Returns:
+            SentimentAnalyzer: The language-specific sentiment analyzer instance.
+        """
         if language not in SentimentAnalyzerFactory._analyzers:
             raise ValueError(f"Unsupported language: {language}")
         return SentimentAnalyzerFactory._analyzers[language]
