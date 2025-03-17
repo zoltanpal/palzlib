c7f07c8e9b83008124f597e8825f2565a1f4675d
Zoltan Pal <palzoltanet@gmail.com>
2025-03-17 15:26:50 +0100

    new mapping class

--- palzlib/database/db_mapping.py
+++ palzlib/database/db_mapping.py
@@ -12,6 +12,13 @@ from palzlib.database.db_client import DBClient
 
 
 class DBMapping:
+    """
+    Initializes the DBMapping class.
+
+    :param db_client: An instance of DBClient to connect to the database.
+    :param mapping_tables: Optional list of table names to be mapped. If None, all tables are mapped.
+    """
+
     def __init__(self, db_client: DBClient, mapping_tables: list = None):
         self.db_client = db_client
         self.mapping_tables = mapping_tables or []
@@ -32,20 +39,37 @@ class DBMapping:
         self.metadata.reflect(self.db_client.engine, **reflection_options)
 
         AutoBase = automap_base(metadata=self.metadata)
+        # reflect the tables
+        AutoBase.prepare(self.db_client.engine)
+
+        """ Optional
         AutoBase.prepare(
             name_for_collection_relationship=self._name_for_collection_relationship,
             generate_relationship=self._generate_relationship,
         )
+        """
 
         self.sorted_tables = [table.name for table in self.metadata.sorted_tables]
         self.db_classes = AutoBase.classes
 
     def __getattr__(self, item: str):
+        """
+        Allows attribute-style access to mapped classes.
+
+        :param item: The name of the mapped class.
+        :return: The corresponding mapped class.
+        """
         if self.db_classes and item in self.db_classes:
             return getattr(self.db_classes, item)
         raise AttributeError(f"Attribute '{item}' not found in mapped classes.")
 
     def __getitem__(self, item: str):
+        """
+        Allows dictionary-style access to mapped classes.
+
+        :param item: The name of the mapped class.
+        :return: The corresponding mapped class.
+        """
         if self.db_classes and item in self.db_classes:
             return self.db_classes[item]
         raise KeyError(f"Key '{item}' not found in mapped classes.")
@@ -54,7 +78,12 @@ class DBMapping:
         return self.db_classes.get(item, default) if self.db_classes else default
 
     @property
-    def mapped_table_names(self):
+    def mapped_table_names(self) -> List[str]:
+        """
+        Returns a list of mapped table names.
+
+        :return: List of mapped SQLAlchemy Table objects.
+        """
         return (
             [cls.__table__ for cls in self.db_classes.values()]
             if self.db_classes
@@ -65,6 +94,10 @@ class DBMapping:
     def _generate_relationship(
         base, direction, return_fn, attr_name, local_cls, referred_cls, **kw
     ):
+        """
+        Customizes the generation of relationships between mapped classes.
+        """
+
         if direction is interfaces.ONETOMANY:
             kw["cascade"] = "all, delete"
             kw["passive_deletes"] = False
@@ -74,6 +107,10 @@ class DBMapping:
 
     @staticmethod
     def _name_for_collection_relationship(base, local_cls, referred_cls, constraint):
+        """
+        Generates a name for collection relationships.
+        """
+
         return (
             constraint.name.lower()
             if constraint.name
